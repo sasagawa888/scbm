9aa38119348f19446646d607e0cc3633ec734274
sasagawa888 <ken-s@tkcnf.or.jp>
2026-09-23 07:41:44 +0900

    add Ssnap_shot debug tool

--- jump.h
+++ jump.h
@@ -674,6 +674,14 @@ static inline void Spush_back(void *cont, int arglist, int th)
     back_goto[rp[th]][th] = cont;
 }
 
+static inline void Ssnap_shot(int th)
+{
+    fprint("rp=%d np=%d sp=%d wp=%d ac=%d \n" ,
+    rp[th],np[th],Jget_sp(th),Jget_wp(th),Jget_ac(th)
+    );
+    Jprint(back_stack[rp[th]][ARGLIST_SCBM][th]);
+    fprint("\n");
+}
 
 
 static inline void Sinc_choice(int th)
